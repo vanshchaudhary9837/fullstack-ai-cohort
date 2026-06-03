38bbb79eddc9dcf703f8fea48370316cf65a72b0
Vansh Chaudhary <vanshchaudhary9837@gmail.com>
2026-06-03 22:53:56 +0530

    Async JS

---
+++ JS/Lectures/Phase5/phase5.js
@@ -0,0 +1,9 @@
+// Synchronous vs Asynchronous
+// Sync -  order mai chalna
+
+
+// GEC -  Global execution context
+// GEC has 1. Memory creation and 2. Code Execution
+// Call Stack -  
+
+console.log("Async JS");
